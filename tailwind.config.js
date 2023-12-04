1e1ac0171461d527d24fded8f9979bde7bc76af6
Twin <scientists.twin1@gmail.com>
2023-12-04 15:15:17 +0400

    feature: apply tailwind to file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
 module.exports = {
-  content: ["./src/**/*.{html,tsx}"],
+  content: ['./src/**/*.{html,tsx}'],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
