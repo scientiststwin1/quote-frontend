39692eb6b617c2884f513272d773644185353614
Twin <scientists.twin1@gmail.com>
2023-12-04 15:15:53 +0400

    feature: create basic button component

---
+++ src/components/atoms/button/button.tsx
@@ -0,0 +1,17 @@
+type ButtonProps = {
+  text: string;
+  onClick: () => void;
+};
+
+const Button: React.FC<ButtonProps> = (props) => {
+  return (
+    <button
+      onClick={props.onClick}
+      className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+    >
+      {props.text}
+    </button>
+  );
+};
+
+export default Button;
