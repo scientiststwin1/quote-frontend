52ea2678806fda0516f3205ef967bd569c8e4a3e
Twin <scientists.twin1@gmail.com>
2023-12-04 17:45:13 +0400

    feature: add quote and quotes component

---
+++ src/components/molecules/quotes/quote.tsx
@@ -0,0 +1,34 @@
+import { memo, useState } from 'react';
+import Button from '../../atoms/button/button';
+
+export type QuoteProps = {
+  id: string;
+  text: string;
+  author: string;
+  onDelete: (id: string) => void;
+  onEdit: (id: string) => void;
+};
+
+const Quote: React.FC<QuoteProps> = (props) => {
+  const [isTextHovered, setTextHovered] = useState(false);
+
+  return (
+    <li
+      onMouseEnter={() => setTextHovered(true)}
+      onMouseLeave={() => setTextHovered(false)}
+      className="flex flex-col justify-center items-center w-full border-b-indigo-600 border-b"
+    >
+      <p className="w-full text-gray-800">{props.text}</p>
+      <span className="self-end font-semibold">{props.author}</span>
+
+      {isTextHovered && (
+        <div className="w-full space-x-2 mb-3">
+          <Button text="Delete" onClick={() => props.onDelete(props.id)} />
+          <Button text="Edit" onClick={() => props.onEdit(props.id)} />
+        </div>
+      )}
+    </li>
+  );
+};
+
+export default memo(Quote);
---
+++ src/components/molecules/quotes/quotes.tsx
@@ -0,0 +1,17 @@
+import Quote, { QuoteProps } from './quote';
+
+type QuotesProps = {
+    quotes: QuoteProps[]
+};
+
+const Quotes: React.FC<QuotesProps> = (props) => {
+  return (
+    <ul className="py-2 space-y-4">
+        {props.quotes.map((quote) => (
+            <Quote key={quote.id} {...quote} />
+        ))}
+    </ul>
+  );
+};
+
+export default Quotes;
