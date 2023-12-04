f7f0d742569754b6c53289e72747788a6d12a30c
Twin <scientists.twin1@gmail.com>
2023-12-04 18:47:25 +0400

    feature: add value to props

--- src/components/atoms/input/input.tsx
+++ src/components/atoms/input/input.tsx
@@ -1,11 +1,13 @@
 export type InputProps = {
   type: string;
-  onChange: () => void;
+  value: string;
+  onChange: (event: any) => void;
 };
 
 const Input: React.FC<InputProps> = (props) => {
   return (
     <input
+      value={props.value}
       type={props.type}
       onChange={props.onChange}
       className="block w-full flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600"
--- src/components/atoms/textArea/textArea.tsx
+++ src/components/atoms/textArea/textArea.tsx
@@ -1,10 +1,15 @@
 export type TextAreaProps = {
-  text: string;
+  value: string;
+  onChange: (event: any) => void;
 };
 
 const TextArea: React.FC<TextAreaProps> = (props) => {
   return (
-    <textarea className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600"></textarea>
+    <textarea
+      value={props.value}
+      onChange={props.onChange}
+      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600"
+    ></textarea>
   );
 };
 
